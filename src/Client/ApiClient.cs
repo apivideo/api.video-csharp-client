a345d9221bf4d226cbb69f6912cd2679b1933ac0
olivierapivideo <olivierapivideo@users.noreply.github.com>
2021-10-04 13:54:38 +0000

    fix: add 'type' attribute in getStatus qualities

--- src/Client/ApiClient.cs
+++ src/Client/ApiClient.cs
@@ -57,7 +57,7 @@ namespace ApiVideo.Client
         /// <param name="basePath">the api base path.</param>
         public ApiClient(string basePath) {
             this.RestClient = new RestClient(basePath);
-            this.RestClient.UserAgent = "api.video client (C#; v:0.0.11; )";
+            this.RestClient.UserAgent = "api.video client (C#; v:0.0.12; )";
         }
         
         /// <summary>
@@ -67,7 +67,7 @@ namespace ApiVideo.Client
         /// <param name="basePath">the api base path.</param>
         public ApiClient(string apiKey,string basePath) {
             this.RestClient = new RestClient(basePath);
-            this.RestClient.UserAgent = "api.video client (C#; v:0.0.11; )";
+            this.RestClient.UserAgent = "api.video client (C#; v:0.0.12; )";
             this.AuthManager = new AuthenticationManager(apiKey, this);
         }
 
@@ -77,7 +77,7 @@ namespace ApiVideo.Client
         /// <param name="client">a RestClient instance used to make API call</param>
         public ApiClient(RestClient client) {
             this.RestClient = client;
-            this.RestClient.UserAgent = "api.video client (C#; v:0.0.11; )";
+            this.RestClient.UserAgent = "api.video client (C#; v:0.0.12; )";
         }
 
         /// <summary>
